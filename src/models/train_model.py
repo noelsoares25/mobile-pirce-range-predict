1c84318ace5f07faa376c0cb3f1127a804134ef0
Noel Soares <noelsoares408@gmail.com>
2024-08-10 16:12:30 +0530

    model created and saved

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,3 +1,8 @@
+import pathlib
+import sys
+import joblib
+import mlflow
+
 from src.features.feature_selection import get_features
 import pandas as pd
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
@@ -34,13 +39,24 @@ def find_best_model_with_params(X_train, y_train, X_test, y_test):
     
     return model, best_p, acc
 
+
+def save_model(model, output_path):
+    # Save the trained model to the specified output path
+    joblib.dump(model, output_path + "/model.joblib")
+
 def main():
+    
+    curr_dir = pathlib.Path(__file__)
+    home_dir = curr_dir.parent.parent.parent
+    output_path = home_dir.as_posix() + "/models"
+    pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
+    
     X,y = get_features()
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
-    model, best_p, acc = find_best_model_with_params(X_train, y_train, X_test, y_test)
-    print(model, best_p, acc)
-    # save_model(trained_model, output_path)   
+    trained_model, best_p, acc = find_best_model_with_params(X_train, y_train, X_test, y_test)
+    print(trained_model, best_p, acc)
+    save_model(trained_model, output_path)   
     # We will push this model to S3 and also copy in the root folder for Dockerfile to pick
 
 
